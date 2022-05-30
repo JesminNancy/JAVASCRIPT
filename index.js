bb9c298a570f27400acc5a39fd214dbfc16022ff
JesminNancy <jesminnancy@hotmail.com>
2022-05-30 18:37:08 +0600

    Shapes & Temperature Converter.

---
+++ index.js
@@ -0,0 +1,21 @@
+// Area of Various Shapes
+
+/* var base = prompt("Enter Base Number : ");
+var height = prompt("Enter Height : ");
+var area = base * height ;
+
+document.write("Area = " + area);*/
+
+// Temperature Converter
+
+/* var farn = prompt("Enter Fahrenheit Number : ");
+
+var cel = (farn-32) * (5/9);
+
+document.write("Celsius is " + cel); */
+
+var cel = prompt("Enter Celsius Number : ");
+
+var farn = (cel*9/5) + 32;
+
+document.write("Celsius is " + farn);

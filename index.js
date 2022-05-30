2b62005ff9a91764980d6e0c85f3929bf29df2d3
JesminNancy <jesminnancy@hotmail.com>
2022-05-30 21:54:26 +0600

    Logical Operator

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-//Display Even/Odd Number
-
-var num = prompt("Enter A Number : ");
-
-if(num%2 == 0)
-document.write("Even");
-if(num%2 !=0)
-document.write("Odd");
-
 // If Else if Else
 
 var marks = prompt("Enter Your Marks : ");
 
-if(marks>=80)
+if(marks>100 || marks<0)
+  document.write("Invalid Number");
+else if(marks>=80 && marks<=100)
   document.write("Your Grade is A+");
-else if(marks>=70)
+else if(marks>=70 && marks<=79)
   document.write("Your Grade is A");
-else if(marks>=60)
+else if(marks>=60 && marks<=69)
   document.write("Your Grade is A-");
-else if(marks>=50)
+else if(marks>=50 && marks<=59)
   document.write("Your Grade is B");
-else if(marks>=40)
+else if(marks>=40 && marks<=49)
   document.write("Your Grade is C");
-else if(marks>=33)
+else if(marks>=33 && marks<=39)
   document.write("Your Grade is D");
 else
   document.write("You Are Failed");
+  
+Vowel / Consonant
+
+var letter = prompt("Enter a letter");
+  letter = letter.toLowerCase();
+if(letter=="a" || letter=="e" || letter=="u" || letter=="0" || letter=="i")
+document.write("Vowel");
+else
+document.write("Constant");
 

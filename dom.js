03a4aba826e867d927445c179d3cba849ca1e34f
JesminNancy <jesminnancy@hotmail.com>
2022-06-03 19:01:59 +0600

    Event Handler to onclick event

---
+++ dom.js
@@ -0,0 +1,20 @@
+
+//  var myVar = document.querySelector("#para");
+// function myMessage1(){
+//   myVar.innerHTML = "You have cilked on button 1";
+  
+// }
+// function myMessage2(){
+//   myVar.innerHTML = "You have cilked on button 2";
+  
+// }
+
+
+var myVar = document.querySelector("#picture");
+function myMessage1(){
+    myVar.src = "images/bcd.jpg";
+    
+   }
+function myMessage2(){
+  myVar.src = "images/img.jpg"; 
+}

78e6788ba668766d14262f7f413d57bd9828027d
JesminNancy <jesminnancy@hotmail.com>
2022-06-04 10:55:22 +0600

    Event Listener

--- dom.js
+++ dom.js
@@ -1,10 +1,25 @@
 
-var myVar = document.getElementById("paraID");
+// var myVar = document.getElementById("paraID");
 
-function addstyle(){
+// function addstyle(){
+//   myVar.classList.add("para_style");
+// }
+
+// function removestyle(){ 
+//   myVar.classList.remove("para_style");
+// }
+
+
+var myVar = document.querySelector("h1");
+
+myVar.addEventListener("mouseover", function(){
+
   myVar.classList.add("para_style");
-}
 
-function removestyle(){ 
+});
+
+myVar.addEventListener("mouseout", function(){
+
   myVar.classList.remove("para_style");
-}
+
+});

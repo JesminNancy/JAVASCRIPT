c84ba0535913de3dc77fe25ddcbe6b94a0e26382
JesminNancy <jesminnancy@hotmail.com>
2022-06-05 21:25:04 +0600

    Event Object  onsubmit event

--- dom.js
+++ dom.js
@@ -1,44 +1,27 @@
-//Input Text Event Object
+//Form Event Object (submit)
 
-// const input = document.querySelector("input");
-// input.addEventListener("change", changeHandler)
+// finding the elements
+const form = document.querySelector("form");
+const name = form.querySelector("div #name");
+const email = form.querySelector("div #email");
+const password = form.querySelector("div #password");
 
-// function  changeHandler(e){
-//      console.log(e);
-//      console.log(e.type);
-//      console.log(e.target);
-//       console.log(e.target.className);
-//      console.log(e.target.id);
-//      console.log(e.target.value);
-// }
+form.addEventListener("submit", formHandler);
 
-//Input Checkbox Event Object
+function formHandler(e) {
+  e.preventDefault();
 
-// const programs = document.querySelectorAll("input[name=program]");
+  const userInfo = {
+    name: name.value,
+    email: email.value,
+    password: password.value,
+  };
 
-// console.log(programs);
-
-// Array.from(programs).map((program)=> {
-//   program.addEventListener("change",programHandler)
-// });
-
-// function programHandler(e){
-//   if (e.target.checked) {
-//   console.log(e.target.value);
-//   }
-// }
-
-const department = document.querySelector("#department");
-// console.log(department);
-
-department.addEventListener("change", departmentHandler);
-
-function departmentHandler(e) {
-  console.log(e.target.value);
+  console.log(userInfo);
+  name.value = "";
+  email.value = "";
+  password.value = "";
 }
-
-
-
 
 
 

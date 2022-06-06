3c144e29d15511744009c5ac33712edf3c99f743
JesminNancy <jesminnancy@hotmail.com>
2022-06-06 18:21:32 +0600

    all mouse events

--- dom.js
+++ dom.js
@@ -1,32 +1,59 @@
-// load, unload,
-// scroll
-// resize
-// toggle - details
+// MouseEvent - <html>, <head>, <meta>, <title>, <br>, <style>, <script>, <iframe>, <param>, <base>, <bdo>
+// 1. onclick
+// 2. ondblclick
+// 3. onmousedown
+// 4. onmousedup
+// 5. onmouseenter
+// 6. onmouseleave
+// 7. onmousemove
+// 8. onmouseover
 
-//  window.addEventListener("load", function () {
-//    console.log("load");
+//const div = document.querySelector("div");
+ //div.addEventListener("click", function(e){
+//  console.log("click is occured");
+//  console.log(e.target);
+// console.log(e.target.id);
+// console.log(e.target.className);
+// console.log(e.target.innerHTML);
+ // console.log(e.target.innerText);
+ //  console.log(e.target.textContent);
+ // });
+
+// div.addEventListener("dblclick", function () {
+//     console.log("dblclick is occured");
 //  });
-//  window.addEventListener("unload", function () {
-//   console.log("unload");
+
+//  div.addEventListener("mousedown", function () {
+//    console.log("mousedown is occured");
+//  });
+  
+// div.addEventListener("mouseup", function () {
+//   console.log("mouseup is occured");
 // });
-//  window.addEventListener("scroll", function () {
-//    console.log("scroll");
-//  });
 
-//  window.addEventListener("resize", function () {
-//    const width = window.outerWidth;
-//    const height = window.outerHeight;
-//    console.log(`height: ${height}, width: ${width}`);
-//  });
-const details = document.querySelector("details");
+// div.addEventListener("mouseenter", function () {
+//   console.log("mouseenter is occured");
+// });
+// div.addEventListener("mouseleave", function () {
+//   console.log("mouseleave is occured");
+// });
 
-details.addEventListener("toggle", function (e) {
-  console.log(e.target.open);
+// div.addEventListener("mousemove", function () {
+//   console.log("mousemove is occured");
+// });
+// div.addEventListener("mouseover", function (e) {
+//   // console.log("mouseover is occured");
+//   console.log("clientX = " + e.clientX + ", clientY = " + e.clientY);
+//   console.log("offsetX = " + e.offsetX + ", offsetY = " + e.offsetY);
+// });
+
+const buttons = document.querySelectorAll(".btn");
+
+Array.from(buttons).map((button) => {
+  button.addEventListener("click", function (e) {
+    console.log(e.target.innerText);
+  });
 });
-
-
-
-
 
 
 

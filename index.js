9ddbb0b65bff34aa0cf555364259e008911588cf
JesminNancy <jesminnancy@hotmail.com>
2022-06-07 20:03:17 +0600

    Browser Object Model  location object

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+var locationDiv = document.querySelector(".location-div");
+var p1 = locationDiv.children[0];
+p1.textContent = location.href;
+var p2 = locationDiv.children[1];
+p2.textContent = location.hostname;
+var p3 = locationDiv.children[2];
+p3.textContent = location.protocol;
+var p4 = locationDiv.children[3];
+p4.textContent = location.port;
 
-document.getElementById("heading2").innerHTML ="Good Bye Good Bye";
 
-document.getElementById("myImg").src ="../JAVASCRIPT/images/world.jpg";
+var vistbtn = document.getElementById("visitbtn");
+vistbtn.addEventListener("click", function(){
+
+location.assign("https://www.youtube.com/");
+
+});

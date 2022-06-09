2e87baf6e20e2099ef15821d3c4d971b0101a086
JesminNancy <jesminnancy@hotmail.com>
2022-06-09 09:00:50 +0600

    for...of and for...in

--- es6.js
+++ es6.js
@@ -1,19 +1,34 @@
 //Object Literals
 
-function studentsinfo(name,age){
-  return{
-    name,
-    age 
-  }
+// function studentsinfo(name,age){
+//   return{
+//     name,
+//     age 
+//   }
   
-}
-// console.log(studentsinfo("Radifa",7));
+// }
+// // console.log(studentsinfo("Radifa",7));
 
-let message = {
-    // body(){
-    'body name'(){
-      return "Hi I am Object Function";
-    }   
+// let message = {
+//     // body(){
+//     'body name'(){
+//       return "Hi I am Object Function";
+//     }   
+// }
+// // console.log(message.body());
+// console.log(message['body name']());
+
+// For..of and for..in
+
+// const names = ["Jorina","Rahim", "Karim"];
+// for(let name of names){
+//   console.log(name);
+// }
+let students = {
+name : "Rahim",
+age : 23,
+cgpa : 4.5
 }
-// console.log(message.body());
-console.log(message['body name']());
+for(let x in students){
+  console.log(`${x} : ${students[x]}`);
+}
